936df2d714b24ed588f74951dfe567b7306ba07c
AbdullahSharbaji <119892603+AbdullahSharbaji@users.noreply.github.com>
2025-06-18 15:29:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def toggle_state():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
-    app.run(host="0.0.0.0", port=port))
+    app.run(host="0.0.0.0", port=port)
